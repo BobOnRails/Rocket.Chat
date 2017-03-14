4abe343de14a7406a6a057652e0b9539a8a7b0db
Bob Dannehl <bob.dannehl@deutschebahn.com>
2017-03-14 12:40:31 +0100

    GPG Test

--- .travis/setupsig.sh
+++ .travis/setupsig.sh
@@ -4,6 +4,6 @@ set -euvo pipefail
 IFS=$'\n\t'
 
 cp .travis/sign.key.gpg  /tmp
-gpg --yes --batch --passphrase=$mypass /tmp/sign.key.gpg
-gpg --allow-secret-key-import --import /tmp/sign.key
-rm /tmp/sign.key
+#gpg --yes --batch --passphrase=$mypass /tmp/sign.key.gpg
+#gpg --allow-secret-key-import --import /tmp/sign.key
+#rm /tmp/sign.key
