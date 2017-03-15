57a4337ed2d46a725eb9b6d16564f034b3731250
Bob Dannehl <bob.dannehl@deutschebahn.com>
2017-03-15 13:37:09 +0100

    gpg

--- .travis/setupsig.sh
+++ .travis/setupsig.sh
@@ -6,5 +6,5 @@ IFS=$'\n\t'
 cp .travis/sign.key.gpg  /tmp
 export mypass="Sm1da,K!" #gpg --gen-key
 gpg --yes --batch --passphrase=$mypass /tmp/sign.key.gpg
-gpg --allow-secret-key-import --import /tmp/sign.key
+gpg --allow-secret-key-import --import /tmp/sign.key.gpg
 rm /tmp/sign.key
