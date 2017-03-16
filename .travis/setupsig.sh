e9cb9372ee8010fbccb85171c5860c7b0665d2bd
Bob Dannehl <bob.dannehl@deutschebahn.com>
2017-03-16 15:30:30 +0100

    test

--- .travis/setupsig.sh
+++ .travis/setupsig.sh
@@ -5,6 +5,8 @@ IFS=$'\n\t'
 
 cp .travis/sign.key.gpg  /tmp
 export mypass="Sm1da,K!" #gpg --gen-key
+echo 'no-tty' >> ~/.gnupg/gpg.conf
+cat ~/.gnupg/gpg.conf
 gpg --yes --batch --passphrase=$mypass /tmp/sign.key.gpg
 gpg --allow-secret-key-import --import /tmp/sign.key.gpg
 # rm /tmp/sign.key
